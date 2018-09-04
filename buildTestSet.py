4c550d3f9f4769ec3cb76abc658fb7a7234a78e3
Linh Hoang Truong <mrthlinh@gmail.com>
2018-09-04 00:07:20 -0500

    build Test Set for RecSys

--- buildTestSet.py
+++ buildTestSet.py
@@ -7,41 +7,15 @@ Created on Tue Aug 28 08:34:00 2018
 """
 
 import pandas as pd
-#import gc #garbage collector
-#import matplotlib.pyplot as plt
-#import dask.dataframe as dd
-#from pandas import pivot_table
 
-#from sklearn import cross_validation as cv
-#from sklearn.metrics.pairwise import pairwise_distances, cosine_distances,cosine_similarity
-#from sklearn.metrics import mean_squared_error
-#from math import sqrt
-#import time
-#import numpy as np
 
 #df_playlists_test = pd.read_hdf('data/df_playlists_test.hdf')
 #df_playlists_test_info = pd.read_hdf('data/df_playlists_test_info.hdf')
     
-# Unique (run on local)
-df_unique_tracks = pd.read_hdf('data/df_tracks.hdf')
-df_tracks = pd.read_hdf('data/df_playlists.hdf')
-df_playlists = pd.read_hdf('data/df_playlists_info.hdf')    
-
-# Run on local with different directory
-#df_unique_tracks = pd.read_hdf('data/df_data/df_tracks.hdf')
-#df_tracks = pd.read_hdf('data/df_data/df_playlists.hdf')
-#df_playlists = pd.read_hdf('data/df_data/df_playlists_info.hdf')   
-
-#- All tracks in the challenge set appear in the MPD
-#- All holdout tracks appear in the MPD
-#
-#The test set contains X difference challenges:
-#- Predict tracks for a playlist given its title and the first 5 tracks
-#- Predict tracks for a playlist given its title and the first 10 tracks
-#- Predict tracks for a playlist given its title and the first 25 tracks
-#- Predict tracks for a playlist given its title and 25 random tracks
-#- Predict tracks for a playlist given its title and the first 100 tracks
-#- Predict tracks for a playlist given its title and 100 random tracks
+# Read data
+df_unique_tracks = pd.read_hdf('data/df_data/df_tracks.hdf')
+df_tracks = pd.read_hdf('data/df_data/df_playlists.hdf')
+df_playlists = pd.read_hdf('data/df_data/df_playlists_info.hdf')    
 
 
 # Pandas table sorted by 
@@ -50,48 +24,195 @@ df_temp['count'] = 1
 df_track_distr = df_temp.groupby(['tid'])['count'].sum().sort_values(ascending=False)
 del df_temp
 
-#criteria_list = [200,100,50,25,10,5]
+#list_size = 5
+#criteria_list = [200,100]
+
+list_size = 1000
 criteria_list = [200,200,100,100,50,50,25,25,10,5]
+
 pid_list = []
+#pid_unfold_list=[]
+df_temp = df_playlists.loc[:,['num_tracks','pid']]
+df_temp = df_temp.set_index('pid')
 for criteria in criteria_list:
     #criteria = 200
-    df_temp = df_playlists.loc[:,['num_tracks','pid']]
+    
     # filter playlists that have more than criteria = 100 tracks 
     df_filter = df_temp[df_temp.num_tracks > criteria]
     
     # Create an empty list to contain the pid
-    list_size = 1000
+    
     list_1000 = []
     while (True):
         # randomly pick 1 pid in df_filter
         ran = df_filter.sample(n=1)
         # get the value of pid
-        pid = ran.pid.values[0]
+        pid = ran.index[0]
+#        pid = ran.pid.values[0]
+                
         # get list of tid (this code uses so many memory)
-        tid_arr=df_tracks[df_tracks.pid == pid].tid
+        tid_arr = df_tracks[df_tracks.pid == pid].tid
         # decrease frequency by 1
         temp = df_track_distr[tid_arr] - 1
         
         if (temp.any() != 0):
             df_track_distr[tid_arr] = temp
             list_1000.append(pid)
+            
+            df_filter = df_filter.drop(pid)
+            
             if (len(list_1000) == list_size):
                 break
-
+            
     pid_list.append(list_1000)
+    df_temp = df_temp.drop(list_1000)
 
-
-#- Predict tracks for a playlist given its 200 random tracks
+# Assemble challenge set
 df_playlists_challenge = pd.DataFrame()
+df_tracks_challenge = pd.DataFrame()
+
 for i in range(len(pid_list)):
     df_temp =  df_playlists[df_playlists.pid.isin(pid_list[i])]
     df_playlists_challenge = pd.concat([df_playlists_challenge,df_temp])
+    
+    df_temp_ =  df_tracks[df_tracks.pid.isin(pid_list[i])]
+    df_tracks_challenge = pd.concat([df_tracks_challenge,df_temp_])
 
-df_playlists_challenge.to_hdf('data/df_playlists_challenge.hdf', key='abc')
+# Assemble training set
+df_playlists_training = df_playlists[~df_playlists.pid.isin(df_playlists_challenge.pid)]
+df_tracks_training= df_tracks[~df_tracks.pid.isin(df_playlists_challenge.pid)]
+
+# Make incomplete playlists
+df_tracks_challenge_incomplete = pd.DataFrame()
+
+# Predict tracks for a playlist given 200 random tracks
+index = 0
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].sample(n=criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given first 200 tracks
+index = 1
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given 100 random tracks
+index = 2
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].sample(n=criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given first 100 tracks
+index = 3
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given 50 random tracks
+index = 4
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].sample(n=criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given first 50 tracks
+index = 5
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+    
+
+# Predict tracks for a playlist given 25 random tracks
+index = 6
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].sample(n=criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+
+
+# Predict tracks for a playlist given first 25 tracks
+index = 7
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+    
+# Predict tracks for a playlist given first 10 tracks
+index = 8
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+    
+# Predict tracks for a playlist given first 5 tracks
+index = 9
+for pid in pid_list[index]:
+    df_temp = df_tracks_challenge[df_tracks_challenge.pid == pid].head(criteria_list[index])
+    df_tracks_challenge_incomplete = pd.concat([df_tracks_challenge_incomplete,df_temp])
+    
+    # Write some test here
+    assert (df_temp.shape[0] < df_tracks_challenge[df_tracks_challenge.pid == pid].shape[0])
+    assert (df_temp.pos.isin(df_tracks_challenge[df_tracks_challenge.pid == pid].pos).all())
+    
+    
+
+#Predict tracks for a playlist given its title and the first 5 tracks
+#Predict tracks for a playlist given its title and the first 10 tracks
+#Predict tracks for a playlist given its title and the first 25 tracks
+#Predict tracks for a playlist given its title and 25 random tracks
+#Predict tracks for a playlist given its title and the first 50 tracks
+#Predict tracks for a playlist given its title and 50 random tracks
+#Predict tracks for a playlist given its title and the first 100 tracks
+#Predict tracks for a playlist given its title and 100 random tracks
+#Predict tracks for a playlist given its title and the first 200 tracks
+#Predict tracks for a playlist given its title and 200 random tracks
+
+# Save file
+df_playlists_challenge.to_hdf('data/df_data/df_playlists_challenge.hdf', key='abc')
+df_tracks_challenge.to_hdf('data/df_data/df_tracks_challenge.hdf', key='abc')
+df_tracks_challenge_incomplete.to_hdf('data/df_data/df_tracks_challenge_incomplete.hdf', key='abc')
 
 
 #print("finish")
 # Write a small test
-#assert(df_playlists_challenge.pid[:10].isin(pid_list[0]).all())
+#assert(df_playlists_challenge.pid[:list_size].isin(pid_list[0]).all())
     
     
