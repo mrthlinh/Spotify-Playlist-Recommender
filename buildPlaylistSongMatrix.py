15c9f4ca2aea29225f769c104e833969e3ab1cf2
Linh Hoang Truong <mrthlinh@gmail.com>
2018-10-08 01:08:06 -0500

    build playlist-song matrix

--- buildPlaylistSongMatrix.py
+++ buildPlaylistSongMatrix.py
@@ -11,14 +11,22 @@ import pandas as pd
 import sys
 from helper import alertError,alertFinishJob
 import gc
-
+import time
     
 def main(argv):
     
     print("Reading Data")
-    df_train = pd.read_hdf('data/df_data/df_train.hdf')
-    df_test = pd.read_hdf('data/df_data/df_test.hdf')
-    df_test_truth = pd.read_hdf('data/df_data/df_test_truth.hdf')
+    df_train = pd.read_hdf('data/df_data/df_train_new.hdf')
+    df_test = pd.read_hdf('data/df_data/df_test_new.hdf')
+    df_test_truth = pd.read_hdf('data/df_data/df_test_truth_new.hdf')
+    
+    df_tracks = pd.read_hdf('data/df_data/df_tracks.hdf')
+
+#    df_train = pd.read_hdf('data/df_data/df_train.hdf')
+#    df_test = pd.read_hdf('data/df_data/df_test.hdf')
+#    df_test_truth = pd.read_hdf('data/df_data/df_test_truth.hdf')
+#    
+#    df_truth = 
    
     # Build playlist-song matrix
     print("Build playlist-song matrix for train set")
@@ -38,11 +46,16 @@ def main(argv):
     pos = df_test_truth.groupby(by='pid')['pos'].apply(list)
     df_ps_test_truth = pd.concat([tid,pos],axis=1)
     
+    print("Build Complate playlist-song matrix")
+    tid = df_tracks.groupby(by='pid')['tid'].apply(list)
+    pos = df_tracks.groupby(by='pid')['pos'].apply(list)
+    df_ps_complete = pd.concat([tid,pos],axis=1)    
     
     print("save matrix Playlist-Songs")
-    df_ps_train.to_hdf("data/df_data/df_playlistSong/df_ps_train.hdf",key='abc')
-    df_ps_test.to_hdf("data/df_data/df_playlistSong/df_ps_test.hdf",key='abc')
-    df_ps_test_truth.to_hdf("data/df_data/df_playlistSong/df_ps_test_truth.hdf",key='abc')
+    df_ps_train.to_hdf("data/df_data/df_playlistSong/df_ps_train_new.hdf",key='abc')
+    df_ps_test.to_hdf("data/df_data/df_playlistSong/df_ps_test_new.hdf",key='abc')
+    df_ps_test_truth.to_hdf("data/df_data/df_playlistSong/df_ps_test_truth_new.hdf",key='abc')
+    df_ps_complete.to_hdf("data/df_data/df_playlistSong/df_ps_complete_new.hdf",key='abc')
     
     del df_ps_train
     del df_ps_test
@@ -52,8 +65,11 @@ if __name__ =="__main__":
 #    parser = argparse.ArgumentParser()
 #    parser.add_argument('--sim_metric', default='cosine', type=str, help='Similarity Metrics')
 #    main(sys.argv)
-    try:
-        main(sys.argv)
-        alertFinishJob("Done")
-    except Exception as e:
-        alertError(str(e))
+    start = time.time()
+    main(sys.argv)
+    print("Time taken = {0:.5f}".format(time.time() - start))
+#    try:
+#        main(sys.argv)
+#        alertFinishJob("Done")
+#    except Exception as e:
+#        alertError(str(e))
--- buildSongPlaylistMatrix.py
+++ buildSongPlaylistMatrix.py
@@ -10,14 +10,16 @@ import pandas as pd
 #import argparse
 import sys
 from helper import alertError,alertFinishJob
-
+import time
     
 def main(argv):
     
     print("Reading Data")
-    df_train = pd.read_hdf('data/df_data/df_train.hdf')
-    df_test = pd.read_hdf('data/df_data/df_test.hdf')
-    df_test_truth = pd.read_hdf('data/df_data/df_test_truth.hdf')
+    df_train = pd.read_hdf('data/df_data/df_train_new.hdf')
+    df_test = pd.read_hdf('data/df_data/df_test_new.hdf')
+    df_test_truth = pd.read_hdf('data/df_data/df_test_truth_new.hdf')
+    
+    df_tracks = pd.read_hdf('data/df_data/df_tracks.hdf')
    
     # Build playlist-song matrix
     print("Build song-playlist matrix for train set")
@@ -37,19 +39,27 @@ def main(argv):
     pos = df_test_truth.groupby(by='tid')['pos'].apply(list)
     df_sp_test_truth = pd.concat([pid,pos],axis=1)
     
+    print("Build Complate playlist-song matrix")
+    tid = df_tracks.groupby(by='tid')['pid'].apply(list)
+    pos = df_tracks.groupby(by='tid')['pos'].apply(list)
+    df_sp_complete = pd.concat([tid,pos],axis=1)   
+    
     
     print("save matrix song-playlist")
-    df_sp_train.to_hdf("data/df_data/df_songPlaylist/df_sp_train.hdf",key='abc')
-    df_sp_test.to_hdf("data/df_data/df_songPlaylist/df_sp_test.hdf",key='abc')
-    df_sp_test_truth.to_hdf("data/df_data/df_songPlaylist/df_sp_test_truth.hdf",key='abc')
-    
+    df_sp_train.to_hdf("data/df_data/df_playlistSong/df_sp_train_new.hdf",key='abc')
+    df_sp_test.to_hdf("data/df_data/df_playlistSong/df_sp_test_new.hdf",key='abc')
+    df_sp_test_truth.to_hdf("data/df_data/df_playlistSong/df_sp_test_truth_new.hdf",key='abc')
+    df_sp_complete.to_hdf("data/df_data/df_playlistSong/df_sp_complete_new.hdf",key='abc')
         
 if __name__ =="__main__":
 #    parser = argparse.ArgumentParser()
 #    parser.add_argument('--sim_metric', default='cosine', type=str, help='Similarity Metrics')
 #    main(sys.argv)
-    try:
-        main(sys.argv)
-        alertFinishJob("Done")
-    except Exception as e:
-        alertError(str(e))
+    start = time.time()
+    main(sys.argv)
+    print("Time taken = {0:.5f}".format(time.time() - start))
+#    try:
+#        main(sys.argv)
+#        alertFinishJob("Done")
+#    except Exception as e:
+#        alertError(str(e))
